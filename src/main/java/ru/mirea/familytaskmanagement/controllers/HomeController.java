ac93eb0bbe22dad55624f424abd22a5be991514f
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2024-11-01 12:58:41 +0300

    Добавил модальное окно

--- src/main/java/ru/mirea/familytaskmanagement/controllers/HomeController.java
+++ src/main/java/ru/mirea/familytaskmanagement/controllers/HomeController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.mirea.familytaskmanagement.models.Task;
+import ru.mirea.familytaskmanagement.models.User;
+import ru.mirea.familytaskmanagement.models.UserRole;
 import ru.mirea.familytaskmanagement.services.TaskService;
+import ru.mirea.familytaskmanagement.services.UserService;
 
 import java.util.List;
 
@@ -13,10 +16,19 @@ import java.util.List;
 public class HomeController {
     @Autowired
     TaskService taskService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/tasks")
     public String getTasks(Model model) {
         List<Task> tasks = taskService.getTasks();
+        User user = userService.getCurrentUser();
+        if (user.getRole() == UserRole.CHILD) {
+            model.addAttribute("role", "Ребенок");
+        } else {
+            model.addAttribute("role", "Родитель");
+        }
+        model.addAttribute("username", user.getUsername());
         model.addAttribute("tasks", tasks);
         return "tasks";
     }}
